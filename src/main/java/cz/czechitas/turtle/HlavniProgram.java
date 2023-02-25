6a0efbfb28c4809ef3a61cfca277543199c2e820
AdelaLindner <a.lindnerova@seznam.cz>
2023-02-25 17:59:29 +0100

    Metody s proměnnou

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -9,51 +9,64 @@ public class HlavniProgram {
 
     public void start() {
 
+        Color cernaBarva;
+        cernaBarva = new Color(16, 14, 14);
+
         Color cervenaBarva;
-        cervenaBarva=new Color(255,0,0);
+        cervenaBarva = new Color(255, 0, 0);
 
         Color modraBarva;
-        modraBarva=new Color(0, 55, 255);
+        modraBarva = new Color(0, 55, 255);
 
         Color zelenaBarva;
-        zelenaBarva=new Color(133, 231, 70);
+        zelenaBarva = new Color(133, 231, 70);
 
         Color zlutaBarva;
-        zlutaBarva=new Color(252, 222, 69);
+        zlutaBarva = new Color(252, 222, 69);
 
         //Zmrzlina:
-        zofka.setLocation(250.0,150);
+        zofka.setLocation(250.0, 150);
         nakresliKruh(1.2, zlutaBarva);
         zofka.turnLeft(90);
         nakresliRovnostrannyTrojuhehlnik(140, cervenaBarva);
 
         //Sněhulák:
-        zofka.setLocation(500.0,150.0);
+        zofka.setLocation(500.0, 150.0);
         nakresliKruh(0.8, modraBarva);
-        zofka.setLocation(520.0,280.0);
+        zofka.setLocation(520.0, 280.0);
         nakresliKruh(1.2, modraBarva);
-        zofka.setLocation(540.0,450.0);
-       nakresliKruh(1.6, modraBarva);
-        zofka.setLocation(580.0,280.0);
+        zofka.setLocation(540.0, 450.0);
+        nakresliKruh(1.6, modraBarva);
+        zofka.setLocation(580.0, 280.0);
         nakresliKruh(0.5, modraBarva);
-        zofka.setLocation(380.0,280.0);
+        zofka.setLocation(385.0, 280.0);
         nakresliKruh(0.5, modraBarva);
 
         //Mašinka:
-        zofka.setLocation(750.0,150.0);
-
-
-
+        zofka.turnLeft(30);
+        zofka.setLocation(900.0, 300.0);
+        nakresliObdelnik(75, 150, zelenaBarva);
+        zofka.turnRight(90);
+        nakresliObdelnik(100, 150, zelenaBarva);
+        zofka.move(100);
+        zofka.turnLeft(90);
+        nakresliKruh(0.8, cervenaBarva);
+        zofka.setLocation(800.0, 335.0);
+        nakresliKruh(0.4, cervenaBarva);
+        zofka.setLocation(880.0, 335.0);
+        nakresliKruh(0.4, cervenaBarva);
+        zofka.setLocation(745.0, 320);
+        nakresliPrvouhlyTrojuhelnik(75, cernaBarva);
 
     }
 
     public void nakresliRovnostrannyTrojuhehlnik(double delkaStrany, Color barvaCary) {
 
-            zofka.setPenColor(barvaCary);
-            zofka.move(delkaStrany);
-            zofka.turnLeft(120);
-            zofka.move(delkaStrany);
-            zofka.turnLeft(120);
+        zofka.setPenColor(barvaCary);
+        zofka.move(delkaStrany);
+        zofka.turnLeft(120);
+        zofka.move(delkaStrany);
+        zofka.turnLeft(120);
         zofka.move(delkaStrany);
     }
 
@@ -65,13 +78,13 @@ public class HlavniProgram {
         }
     }
 
-    public void nakresliObdelnik(double delkaDelsiStrany, double delkaKratsiStrany, Color barvaCary) {
+    public void nakresliObdelnik(double delkaStranaA, double delkaStranaB, Color barvaCary) {
 
-        zofka.setPenColor(barvaCary);
+        turtle.setPenColor(barvaCary);
         for (int i = 0; i < 2; i++) {
-            zofka.move(delkaKratsiStrany);
+            zofka.move(delkaStranaA);
             zofka.turnLeft(90);
-            zofka.move(delkaDelsiStrany);
+            zofka.move(delkaStranaB);
             zofka.turnLeft(90);
         }
     }
@@ -84,6 +97,16 @@ public class HlavniProgram {
         }
     }
 
+    public void nakresliPrvouhlyTrojuhelnik(double delkaOdvesny, Color barvaCary) {
+        zofka.setPenColor(barvaCary);
+        var delkaPrepony = Math.sqrt(2 * Math.pow(delkaOdvesny, 2));
+        zofka.move(delkaOdvesny);
+        zofka.turnLeft(135);
+        zofka.move(delkaPrepony);
+        zofka.turnLeft(135);
+        zofka.move(delkaOdvesny);
+
+    }
 
     public static void main(String[] args) {
         new HlavniProgram().start();
